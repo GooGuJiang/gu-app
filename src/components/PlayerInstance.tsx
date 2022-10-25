db7f66406feac918e4cb89687aa9956c2e58bc3f
WindLeaf233 <mc.windleaf@foxmail.com>
2022-10-25 13:20:15 +0800

    :adhesive_bandage: 用事件获取 DOM 而非直接获取

--- src/components/PlayerInstance.tsx
+++ src/components/PlayerInstance.tsx
@@ -7,16 +7,16 @@ export default function PlayerInstance({ setTime }: { setTime: Function }) {
       src="./music/HoneyComeBear - 走馬灯.mp3"
       listenInterval={1}
       onPause={e => {
-        const audio = document.querySelector('audio');
-        setTime(audio!.currentTime*1000+500);
+        let target = e.target as HTMLAudioElement;
+        setTime(target!.currentTime * 1000 + 500);
         console.log("暂停");
       }}
       onPlay={e => {
         console.log("开始播放");
       }}
       onListen={e => {
-        const audio = document.querySelector('audio');
-        setTime((audio!.currentTime*1000+500));
+        let target = e.target as HTMLAudioElement;
+        setTime((target!.currentTime * 1000 + 500));
         //console.log(audio!.currentTime);
         //</>const test = documgetElementById("rhap_current-time")
         // .textContent;m^Kl0(T7Q(SX76%((XKT
